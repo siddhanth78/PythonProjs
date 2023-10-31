31ac74e9e87e649dcb1f1b6e3071167d73d97830
siddhanth78 <siddhanth.lalgowdar@gmail.com>
2023-10-30 19:09:41 -0600

    Added rem num sym and capitalize

--- bulkRename.py
+++ bulkRename.py
@@ -9,12 +9,13 @@ def getfiles():
     root.withdraw()
     file_paths = fd.askopenfilenames()
     files = []
-    i = 1
+    count = 0
     for fp in file_paths:
         filepath = Path(fp)
         print(filepath)
-        files.append((filepath, f"{i}"))
-        i+=1
+        count += 1
+        files.append((filepath, f"{count}"))
+    print(f"{count} files selected.")
     return files
 
 def renameFile(file):
@@ -23,16 +24,46 @@ def renameFile(file):
     name_ = file[2] + file[1] + suffix_
     old = file[0]
     new_path = Path(dir_, name_)
+    if Path.exists(new_path):
+        print(f"File {new_path.stem} exists. Skipping.")
     old.rename(new_path)
-    print(new_path)
     
-def pool_handler():
+def removeSym(file):
+    dir_ = file[0].parent
+    suffix_ = file[0].suffix
+    name_ = "".join(f for f in file[0].stem if f.isalpha()) + suffix_
+    old = file[0]
+    new_path = Path(dir_, name_)
+    if Path.exists(new_path):
+        print(f"File {new_path.stem} exists. Skipping.")
+        return
+    old.rename(new_path)
+    
+def toCap(file):
+    dir_ = file[0].parent
+    suffix_ = file[0].suffix
+    name_ = file[0].stem.upper() + suffix_
+    old = file[0]
+    new_path = Path(dir_, name_)
+    old.rename(new_path)
+    
+def pool_handler(mode):
     p = Pool(4)
-    p.map(renameFile, replace)
+    if mode in "rename":
+        p.map(renameFile, replace)
+        print("Rename complete.")
+    elif mode in "rem num sym":
+        p.map(removeSym, files)
+        print("Numbers and symbols removed.")
+    elif mode in "capitalize":
+        p.map(toCap, files)
+        print("Capitalization complete.")
 
 
 if __name__ == '__main__':
     files = getfiles()
-    new_name = input("Rename: ")
-    replace = [(f[0], f[1], new_name) for f in files]
-    pool_handler()
+    choice = input("func: ")
+    if choice in "rename":
+        new_name = input("new name: ")
+        replace = [(f[0], f[1], new_name) for f in files]
+    pool_handler(choice)
